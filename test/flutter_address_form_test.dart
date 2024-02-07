557d28f7bee1bcf013cd5e2b98feabe3714a9588
mike doornenbal <mikedoornenbal9@icloud.com>
2024-02-07 15:02:38 +0100

    fix: added CI and Linter

--- test/flutter_address_form_test.dart
+++ test/flutter_address_form_test.dart
@@ -2,13 +2,12 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutter_address_form/flutter_address_form.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
+    var calculator = Calculator();
     expect(calculator.addOne(2), 3);
     expect(calculator.addOne(-7), -6);
     expect(calculator.addOne(0), 1);
